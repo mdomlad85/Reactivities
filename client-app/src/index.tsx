d970379f76b38ea8abce04a691d663606c9308bf
marko.domladovac <marko.domladovac@epti.com>
2022-10-12 16:31:01 +0200

    Adding MobX store and refactoring code to use store for states

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import {store, StoreContext} from "./app/stores/store";
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+const container =  document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+    <StoreContext.Provider value={store}>
+        <App />
+    </StoreContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
